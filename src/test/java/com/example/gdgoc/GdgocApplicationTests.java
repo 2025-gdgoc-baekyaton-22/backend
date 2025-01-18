36f20be3546bcf986fa12bb65ff76f9a81ad9ef2
g2hy <g2hyeong@gmail.com>
2025-01-18 17:59:22 +0900

    initial setup

---
+++ src/test/java/com/example/gdgoc/GdgocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gdgoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdgocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
