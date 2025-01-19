756fbc4688678328ea56c5c89e85acaec0e533ad
Hyun0828 <spdlqj4818@naver.com>
2025-01-19 09:17:23 +0900

    api 연결 해제

--- src/main/java/com/example/gdgoc/service/SendMessageService.java
+++ src/main/java/com/example/gdgoc/service/SendMessageService.java
@@ -31,63 +31,63 @@ public class SendMessageService {
         this.userService = userService;
     }
 
-    @Scheduled(cron = "0 */50 * * * *")
-    public void sendMessage() {
-        userService.findAllUserData().stream()
-                .filter(data -> !data.isThreeOrTwo())
-                .forEach(data -> {
-                    String text = String.format("http://dollbomzigi.store/survey-index 로 접속하세요!", data.getId());
-                    sendMessage(data.getCareGiverPhone(), data.getCareTakerPhone(), text);
-                });
-    }
+//    @Scheduled(cron = "0 */50 * * * *")
+//    public void sendMessage() {
+//        userService.findAllUserData().stream()
+//                .filter(data -> !data.isThreeOrTwo())
+//                .forEach(data -> {
+//                    String text = String.format("http://dollbomzigi.store/survey-index 로 접속하세요!", data.getId());
+//                    sendMessage(data.getCareGiverPhone(), data.getCareTakerPhone(), text);
+//                });
+//    }
 
     // 2번 보내야 되는 로직
 //    @Scheduled(cron = "0 57 4 * * *", zone = "Asia/Seoul")
-    @Scheduled(cron = "0 0 8,20 * * *", zone = "Asia/Seoul") // 매일 08:00, 20:00에 실행
-    public void send2Message() {
-        LocalTime currentTime = LocalTime.now();
-        boolean isMorning = currentTime.getHour() == 8;
-        boolean isEvening = currentTime.getHour() == 20;
-
-        userService.findAllUserData().stream()
-                .filter(data -> !data.isThreeOrTwo())
-                .forEach(data -> {
-                    if (isMorning) {
-                        String text = String.format("http://dollbomzigi.store/survey-index/%d/1 로 접속하세요!",
-                                data.getId());
-                        sendMessage(data.getCareGiverPhone(), data.getCareTakerPhone(), text);
-                    } else if (isEvening) {
-                        String text = String.format("http://dollbomzigi.store/survey-index/%d/3 로 접속하세요!",
-                                data.getId());
-                        sendMessage(data.getCareGiverPhone(), data.getCareTakerPhone(), text);
-                    }
-                });
-    }
-
-    // 3번 보내야 되는 로직
-    @Scheduled(cron = "0 0 8,14,20 * * *", zone = "Asia/Seoul") // 매일 08:00, 14:00, 20:00에 실행
-    public void send3Message() {
-        LocalTime currentTime = LocalTime.now();
-        int hour = currentTime.getHour();
-
-        userService.findAllUserData().stream()
-                .filter(data -> !data.isThreeOrTwo()) // 3번 보내는 로직으로 필터링
-                .forEach(data -> {
-                    if (hour == 8) {
-                        String text = String.format("http://dollbomzigi.store/survey-index/%d/1 로 접속하세요!",
-                                data.getId());
-                        sendMessage(data.getCareGiverPhone(), data.getCareTakerPhone(), text);
-                    } else if (hour == 14) {
-                        String text = String.format("http://dollbomzigi.store/survey-index/%d/2 로 접속하세요!",
-                                data.getId());
-                        sendMessage(data.getCareGiverPhone(), data.getCareTakerPhone(), text);
-                    } else if (hour == 20) {
-                        String text = String.format("http://dollbomzigi.store/survey-index/%d/3 로 접속하세요!",
-                                data.getId());
-                        sendMessage(data.getCareGiverPhone(), data.getCareTakerPhone(), text);
-                    }
-                });
-    }
+//    @Scheduled(cron = "0 0 8,20 * * *", zone = "Asia/Seoul") // 매일 08:00, 20:00에 실행
+//    public void send2Message() {
+//        LocalTime currentTime = LocalTime.now();
+//        boolean isMorning = currentTime.getHour() == 8;
+//        boolean isEvening = currentTime.getHour() == 20;
+//
+//        userService.findAllUserData().stream()
+//                .filter(data -> !data.isThreeOrTwo())
+//                .forEach(data -> {
+//                    if (isMorning) {
+//                        String text = String.format("http://dollbomzigi.store/survey-index/%d/1 로 접속하세요!",
+//                                data.getId());
+//                        sendMessage(data.getCareGiverPhone(), data.getCareTakerPhone(), text);
+//                    } else if (isEvening) {
+//                        String text = String.format("http://dollbomzigi.store/survey-index/%d/3 로 접속하세요!",
+//                                data.getId());
+//                        sendMessage(data.getCareGiverPhone(), data.getCareTakerPhone(), text);
+//                    }
+//                });
+//    }
+//
+//    // 3번 보내야 되는 로직
+//    @Scheduled(cron = "0 0 8,14,20 * * *", zone = "Asia/Seoul") // 매일 08:00, 14:00, 20:00에 실행
+//    public void send3Message() {
+//        LocalTime currentTime = LocalTime.now();
+//        int hour = currentTime.getHour();
+//
+//        userService.findAllUserData().stream()
+//                .filter(data -> !data.isThreeOrTwo()) // 3번 보내는 로직으로 필터링
+//                .forEach(data -> {
+//                    if (hour == 8) {
+//                        String text = String.format("http://dollbomzigi.store/survey-index/%d/1 로 접속하세요!",
+//                                data.getId());
+//                        sendMessage(data.getCareGiverPhone(), data.getCareTakerPhone(), text);
+//                    } else if (hour == 14) {
+//                        String text = String.format("http://dollbomzigi.store/survey-index/%d/2 로 접속하세요!",
+//                                data.getId());
+//                        sendMessage(data.getCareGiverPhone(), data.getCareTakerPhone(), text);
+//                    } else if (hour == 20) {
+//                        String text = String.format("http://dollbomzigi.store/survey-index/%d/3 로 접속하세요!",
+//                                data.getId());
+//                        sendMessage(data.getCareGiverPhone(), data.getCareTakerPhone(), text);
+//                    }
+//                });
+//    }
 
     public void sendMessage(String fromPhoneNumber, String toPhoneNumber, String text) {
         Message message = new Message();
